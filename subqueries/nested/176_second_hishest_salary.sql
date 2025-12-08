0c80ccb6c253b867eb70fa5e6c8efd38d59a86ff
Avreet Kaur <128018306+avreetkaur84@users.noreply.github.com>
2025-12-08 12:44:57 +0530

    Delete subqueries/nested directory

--- subqueries/nested/176_second_hishest_salary.sql
+++
@@ -1,7 +0,0 @@
--- Question Link: https://leetcode.com/problems/second-highest-salary/description/
-
-SELECT MAX(salary) [SecondHighestSalary]
-FROM Employee
-WHERE salary NOT IN (
-    SELECT MAX(salary) FROM Employee
-);
